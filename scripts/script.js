d675c0f76e513b03516395f3334ecc45529aa538
mstanton6 <mstanton6@yahoo.com>
2025-12-17 20:29:46 -0500

    Part 3 working

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,40 @@
+
+// In order to add submenu links, we will need to restructure the menuLinks array within index.js. 
+// Update the menuLinks array to the following:
+/* var menuLinks = [
+
+  {text: 'about', href: '/about'},
+
+  {text: 'catalog', href: '#', subLinks: [
+
+    {text: 'all', href: '/catalog/all'},
+
+    {text: 'top selling', href: '/catalog/top'},
+
+    {text: 'search', href: '/catalog/search'},
+
+  ]},
+
+  {text: 'orders', href: '#' , subLinks: [
+
+    {text: 'new', href: '/orders/new'},
+
+    {text: 'pending', href: '/orders/pending'},
+
+    {text: 'history', href: '/orders/history'},
+
+  ]},
+
+  {text: 'account', href: '#', subLinks: [
+
+    {text: 'profile', href: '/account/profile'},
+
+    {text: 'sign out', href: '/account/signout'},
+
+  ]},
+
+];
+*/
 // Menu data structure
 var menuLinks = [
   { text: "about", href: "/about" },
@@ -68,4 +105,22 @@ subMenuEl.style.backgroundColor = "var(--sub-menu-bg)";
 // 4. Add the class of flex-around to the subMenuEl element.
 subMenuEl.classList.add("flex-around");
 
-/// Part 4: 
+/// Part 3b: Now, change the position of the submenu to temporarily hide it. Later, we will make the submenu appear dynamically based on user interaction:
+// 1. Set the CSS position property of subMenuEl to the value of absolute.
+subMenuEl.style.position = 'absolute';
+
+// 2. Set the CSS top property of subMenuEl to the value of 0.
+subMenuEl.style.top = '0';
+
+/// Part 4: Adding Menu Interaction
+//In order to add interaction:
+
+// 1. Select and cache the all of the <a> elements inside of topMenuEl in a variable named topMenuLinks.
+
+// 2. Attach a delegated 'click' event listener to topMenuEl.
+
+// 2a. The first line of code of the event listener function should call the event object's preventDefault() method.
+
+// 2b. The second line of code of the function should immediately return if the element clicked was not an <a> element.
+
+//2c. Log the content of the <a> to verify the handler is working.
